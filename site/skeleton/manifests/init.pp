78ac83b1663c4ffb5a377d774b24fa500d855c4a
aminsamir <amin.samir@gmail.com>
2016-08-17 08:42:02 +0800

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,8 +1,7 @@
 class skeleton {  
 file { '/etc/skel/.bashrc':
   ensure  => 'file',
-  content => '{md5}2f8222b4f275c4f18e69c34f66d2631b',
-  ctime   => '2016-08-13 00:32:29 +0000',
+  source => 'puppet:///modules/skeleton/.bashrc',
   group   => '0',
   mode    => '0644',
   mtime   => '2015-03-05 22:06:48 +0000',

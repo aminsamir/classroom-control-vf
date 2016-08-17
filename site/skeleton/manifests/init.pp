d1eef8377ef24a7a3250556758fc4f8c741443e6
aminsamir <amin.samir@gmail.com>
2016-08-17 09:07:47 +0800

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,10 +1,13 @@
 class skeleton {  
-file { '/etc/skel/.bashrc':
+file { '/etc/skel':
 ensure => directory,
+}
+file { '/etc/skel/.bashrc':
+ensure => file,
 owner => 'root',
 group => 'root',
 mode => '0440',
-source => 'puppet:///modules/skel/bashrc',
+source => 'puppet:///modules/skelelton/bashrc',
 }
 }
 

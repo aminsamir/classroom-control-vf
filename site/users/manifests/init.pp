533780839ef1079d8223596db5445befcbabcda4
aminsamir <amin.samir@gmail.com>
2016-08-17 08:24:44 +0800

    Update init.pp

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,12 +1,5 @@
 class users {
 user { 'fundamentals':
   ensure           => 'present',
-  gid              => '1000',
-  home             => '/home/fundamentals',
-  password         => '!!',
-  password_max_age => '99999',
-  password_min_age => '0',
-  shell            => '/bin/bash',
-  uid              => '1000',
 }
 }

81695b1e45374d33907a8392751738c138b2e454
aminsamir <amin.samir@gmail.com>
2016-08-17 08:50:50 +0800

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,7 +1,7 @@
 class skeleton {  
 file { '/etc/skel/.bashrc':
   ensure  => 'file',
-  source => 'puppet:///modules/skeleton/.bashrc',
+  source => 'puppet:///modules/skeleton/bashrc',
   type    => 'file',
 }
 }
